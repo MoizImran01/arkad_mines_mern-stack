9896670c987cc1c11302ba8ca07e09e8dab36288
MoizImran01 <moizimran832@gmail.com>
2025-10-23 21:52:36 +0500

    Refactor backend structure for Vercel

--- ARKAD_Mines_backend/api/index.js
+++ ARKAD_Mines_backend/api/index.js
@@ -1,17 +1,16 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv"; 
-import { connectDB } from "../config/db.js";
-import userRouter from "../Routes/userRouter.js";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { connectDB } = require("../config/db.js");
+const userRouter = require("../Routes/userRouter.js");
 
 dotenv.config({ path: './config.env' });
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
-
 
 connectDB();
 app.use("/api/user", userRouter);
@@ -20,6 +19,4 @@ app.get("/", (req, res) => {
     res.send("API Working");
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-});
+module.exports = app;
