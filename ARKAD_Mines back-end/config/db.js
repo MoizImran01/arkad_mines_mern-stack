54c80639d0d18228767f6eed2651b83282690bd8
MoizImran01 <moizimran832@gmail.com>
2025-10-23 21:06:32 +0500

    Refactor backend structure for Vercel

--- ARKAD_Mines back-end/config/db.js
+++ ARKAD_Mines back-end/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected:", mongoose.connection.name);
   } catch (err) {
-    console.error('MongoDB Connection Error:', err.message);
+    console.error("MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 };
