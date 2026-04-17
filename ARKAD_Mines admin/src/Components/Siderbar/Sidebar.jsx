b4834a3fd83ff9d6c55ce7acb8c10c0eb008b451
raahima <raahimausman2003@gmail.com>
2026-04-18 03:32:48 +0500

    fix

--- ARKAD_Mines admin/src/Components/Siderbar/Sidebar.jsx
+++ ARKAD_Mines admin/src/Components/Siderbar/Sidebar.jsx
@@ -12,50 +12,68 @@ const Siderbar = () => {
           <p>Dashboard</p>
         </NavLink>
 
-        <NavLink to='/add' className="sidebar-option">
-          <FiPlusSquare className="sidebar-icon" />
-          <p>Add Items</p>
-        </NavLink>
+        <div className="sidebar-divider" aria-hidden="true" />
 
-        <NavLink to='/list' className="sidebar-option">
-          <FiList className="sidebar-icon" />
-          <p>List Items</p>
-        </NavLink>
+        <div className="sidebar-group">
+          <div className="sidebar-section-header">Inventory</div>
 
-        <NavLink to='/orders' className="sidebar-option">
-          <FiShoppingCart className="sidebar-icon" />
-          <p>Orders</p>
-        </NavLink>
+          <NavLink to='/add' className="sidebar-option">
+            <FiPlusSquare className="sidebar-icon" />
+            <p>Add Items</p>
+          </NavLink>
 
-        <NavLink to='/dispatch' className="sidebar-option">
-          <FiPackage className="sidebar-icon" />
-          <p>Dispatch</p>
-        </NavLink>
+          <NavLink to='/list' className="sidebar-option">
+            <FiList className="sidebar-icon" />
+            <p>List Items</p>
+          </NavLink>
 
-        <NavLink to='/users' className="sidebar-option">
-          <FiUser className="sidebar-icon" />
-          <p>Users</p>
-        </NavLink>
+          <NavLink to='/forecasting' className="sidebar-option">
+            <FiTrendingUp className="sidebar-icon" />
+            <p>Inventory Forecasting</p>
+          </NavLink>
+        </div>
 
-        <NavLink to='/quotes' className="sidebar-option">
-          <FiFileText className="sidebar-icon" />
-          <p>Quotes</p>
-        </NavLink>
+        <div className="sidebar-divider" aria-hidden="true" />
 
-        <NavLink to='/purchase-orders' className="sidebar-option">
-          <FiTruck className="sidebar-icon" />
-          <p>Purchase Orders</p>
-        </NavLink>
+        <div className="sidebar-group">
+          <div className="sidebar-section-header">Orders</div>
 
-        <NavLink to='/create-purchase-order' className="sidebar-option">
-          <FiPackage className="sidebar-icon" />
-          <p>Create Purchase Order</p>
-        </NavLink>
+          <NavLink to='/orders' className="sidebar-option">
+            <FiShoppingCart className="sidebar-icon" />
+            <p>Orders</p>
+          </NavLink>
 
-        <NavLink to='/forecasting' className="sidebar-option">
-          <FiTrendingUp className="sidebar-icon" />
-          <p>Inventory Forecasting</p>
-        </NavLink>
+          <NavLink to='/dispatch' className="sidebar-option">
+            <FiPackage className="sidebar-icon" />
+            <p>Dispatch</p>
+          </NavLink>
+
+          <NavLink to='/quotes' className="sidebar-option">
+            <FiFileText className="sidebar-icon" />
+            <p>Quotes</p>
+          </NavLink>
+
+          <NavLink to='/purchase-orders' className="sidebar-option">
+            <FiTruck className="sidebar-icon" />
+            <p>Purchase Orders</p>
+          </NavLink>
+
+          <NavLink to='/create-purchase-order' className="sidebar-option">
+            <FiPackage className="sidebar-icon" />
+            <p>Create Purchase Order</p>
+          </NavLink>
+        </div>
+
+        <div className="sidebar-divider" aria-hidden="true" />
+
+        <div className="sidebar-group">
+          <div className="sidebar-section-header">Admin</div>
+
+          <NavLink to='/users' className="sidebar-option">
+            <FiUser className="sidebar-icon" />
+            <p>Users</p>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
