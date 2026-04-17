6285843c01d33171d92c6fd11ae23efbd18734ff
raahima <raahimausman2003@gmail.com>
2026-04-18 02:55:30 +0500

    fixx

--- ARKAD_Mines_backend/api/index.js
+++ ARKAD_Mines_backend/api/index.js
@@ -75,7 +75,14 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Cache-Control',
+    'Pragma',
+    'Expires',
+    'X-Requested-With'
+  ]
 }));
 
 app.get("/live", (req, res) => res.status(200).type("text").send("ok"));
